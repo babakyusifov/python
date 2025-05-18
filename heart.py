8471a567a8c26c6fc2ac7a650f5c85c561a9a7c6
Babak <143651892+babakyusifov@users.noreply.github.com>
2025-05-18 16:56:22 +0000

    last vers.

--- heart.py
+++ heart.py
@@ -1,29 +1,32 @@
 import turtle
 import math
 
+# Heart funksiyaları
 def hearta(k):
     return 15 * math.sin(k)**3
 
 def heartb(k):
     return 12*math.cos(k) - 5*math.cos(2*k) - 2*math.cos(3*k) - math.cos(4*k)
 
-turtle.speed(0)
+# Ekranı qur
 turtle.bgcolor("black")
 turtle.color("#f73487")
+turtle.pensize(2)
+turtle.speed(0)
 turtle.penup()
 
-# Use smaller steps and k in radians from 0 to 2*pi
-for i in range(0, 360, 1):
-    k = math.radians(i)
+# Rəsmə başla
+for i in range(0, 360):
+    k = math.radians(i)  # dərəcəni radiana çevir
     x = hearta(k) * 20
     y = heartb(k) * 20
     turtle.goto(x, y)
-    turtle.pendown()
+    if i == 0:
+        turtle.pendown()
 
 turtle.hideturtle()
-
-# Save drawing to file
-ts = turtle.getscreen()
-ts.getcanvas().postscript(file="heart.ps")
-
 turtle.done()
+
+# Rəsmi .eps (və ya .ps) faylına yaz
+ts = turtle.getscreen()
+ts.getcanvas().postscript(file="heart.eps")
